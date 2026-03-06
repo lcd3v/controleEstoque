a5761ed144ed288e6665d378a1fbd0c461655bbd
lcd3v <leonardosantosd3v@gmail.com>
2026-03-05 22:48:13 -0300

    Estilização do Sistema

--- src/main/java/connection/ConectionFactory.java
+++ src/main/java/connection/ConectionFactory.java
@@ -4,33 +4,35 @@
  */
 package connection;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
 /**
  *
  * @author 232.005495
  */
 public class ConectionFactory {
-    
-        private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-        private static final String URL =
-            "jdbc:mysql://localhost:3306/estoque_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-        private static final String USER = "root";
-        private static final String PASSWORD = "cimatec";
-    //Métodos
-        
-        public static Connection getConnection(){
-            Connection con = null;
-            
-            try {
-                class.forName(DRIVER);
-                con = DriverManeger.getconnection(URL, USER, PASSWORD);
-                System.out.println("Banco de dados conectado.");
-            } catch (Exception e) {
-                System.out.println("Banco de dados não conectado");
-                e.printStackTrace();
-            }
-            
-            return con;
-             
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver"; //driver de conexão com o banco
+    private static final String URL
+            = "jdbc:mysql://localhost:3306/estoque_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC"; // url de conexão com o banco
+    private static final String USER = "root"; // usuário do banco
+    private static final String PASSWORD = "leonardod3v"; // senha do banco
+
+    //método pra fazer a conexão com o banco
+    public static Connection getConnection() {
+        Connection con = null; // variável de conexão   
+
+        try {
+            Class.forName(DRIVER); // carrega o driver na memória
+            con = DriverManager.getConnection(URL, USER, PASSWORD); // cria a conexão com o banco usando os parametros url, user e password
+            System.out.println("Banco de dados conectado."); // mensagem de sucesso no console
+        } catch (Exception e) {
+            System.out.println("Banco de dados não conectado"); // mensagem caso ocorra erro
+            e.printStackTrace(); // mostra os detalhes do erro no console
         }
+        return con; // retorna a conexão ou null se falhar 
+    }
 }
-}
+
--- src/main/java/login/Login.java
+++ src/main/java/login/Login.java
@@ -4,25 +4,27 @@
  */
 package login;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-@WebServlet("/login") // caminho
+import jakarta.servlet.ServletException; // trata erros relacionados ao servLet
+import jakarta.servlet.annotation.WebServlet; // permite mapear a URL do servLet
+import jakarta.servlet.http.HttpServlet; // classe base para criar  servLets HTTP
+import jakarta.servlet.http.HttpServletRequest; // requisição do cliente
+import jakarta.servlet.http.HttpServletResponse; // resposta do servidor
+import java.io.IOException; // trata erros de entrada e de saída
+import java.io.PrintWriter; // permite escrever respostas em HTML
+
+@WebServlet("/login") // caminho de acesso da classe
 public class Login extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;  
+    private static final long serialVersionUID = 1L; // identificador de versão da classe (boas práticas)
 
+    // metodo que vai ser executado sempre quando o formulário envia dados usando method="post"
     protected void doPost(HttpServletRequest request, HttpServletResponse response)// fazemos uma requisição e uma resposta no servidor local
             throws ServletException, IOException {
         String usuario = request.getParameter("user"); // aqui falo qual parametro( name ) que uso no html 
         String senha = request.getParameter("passw");//aqui falo qual parametro( name ) que uso no html
         
         response.setContentType("text/html");// eu estou setando de que forma ele vai me responder ( em texto html ) 
-        PrintWriter out = response .getWriter(); // escreve no html, fazendo o get do que será escrito 
+        PrintWriter out = response.getWriter(); // escreve no html, fazendo o get do que será escrito 
                 
         if("admin".equals(usuario)&& "1234".equals(senha)){
             response.sendRedirect("dashBoard.html");// direciona para a pagina
