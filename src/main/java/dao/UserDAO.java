a4cbce241f05203999d70629813d0ac83c01e883
lcd3v <leonardosantosd3v@gmail.com>
2026-03-09 14:29:39 -0300

    codigoCorrigido

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -1,31 +1,30 @@
 package dao;
 
+import connection.ConectionFactory;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import model.UserModel;
 
 public class UserDAO {
-
-    public boolean validarLogin(UserModel userModel) {
-        String sql = "SELECT * FROM users WHERE username= ? AND psw= ?";
-    }
-}
-try (var con = Conection.getConnection()){
-           
-            
-            PreparedStatement stmt = con.prepareStatement(sql);
-            stmt.setString(1, usuario);
-            stmt.setString(2,senha);
-            
-            ResultSet rs = stmt.executeQuery(); 
-            if(rs.next()){
-                response.sendRedirect("Pages/dashBoard.html");
+    
+    public boolean  validarLogin(UserModel userModel) {
+        String sql = 
+                "SELECT * FROM users WHERE username= ? AND psw= ?";
+        
+        try (var con = ConectionFactory.getConnection()){
                         
-            }else{
-                out.println("<h2>Usuario ou senha invalidos</h2>");
-            }
+            PreparedStatement stmt =
+                    con.prepareStatement(sql);
+            stmt.setString(1, userModel.getUsername());
+            stmt.setString(2, userModel.getPassword());
+            
+            ResultSet rs = stmt.executeQuery();
+            
+            return rs.next();
+   
         } catch (Exception e) {
             e.printStackTrace();
-            out.println("<h2>Erro ao conectar com o banco de dados</h2>");
+            return false;
         }
-        
     }
 }
