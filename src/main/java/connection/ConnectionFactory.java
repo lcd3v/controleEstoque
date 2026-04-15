8785d9716decf662dedeffac2953ecc4431da2bf
lcd3v <leonardosantosd3v@gmail.com>
2026-04-15 19:50:58 -0300

    testandoCadastroBanco

--- src/main/java/connection/ConnectionFactory.java
+++ src/main/java/connection/ConnectionFactory.java
@@ -1,40 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package connection;
 
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-/**
- *
- * @author 232.004057
- */
 public class ConnectionFactory {
-    
-    //dados do banco
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL =
-        "jdbc:mysql://db:3306/estoque_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASSWORD = "leonardod3v";
+        private static final String URL = System.getenv("DB_URL");
+        private static final String USER = System.getenv("DB_USER");
+        private static final String PASSWORD = System.getenv("DB_PASS");
+        private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
-    public static Connection getConnection() {
-        Connection con = null;
-        
-        try {
-            Class.forName(DRIVER);
-            con = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Bando de Dados Conectado");
-        } catch (Exception e) {
-            System.out.println("Banco de Dados Não Conectado");
-            e.printStackTrace();
-        }
-        
-        return con;
-    }
+        public static Connection getConnection() {
+            Connection con = null;
             
-    
+            try {
+                if(URL == null || USER == null || PASSWORD == null){
+                    System.out.println("Variável de ambiente com problema");
+                    return null;
+                }
+                
+                Class.forName(DRIVER);
+                
+                con = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Banco de Dados Conectado");
+                
+            }catch (SQLException e) {
+                System.out.println("Erro no SQL");
+                e.printStackTrace();
+            }
+            catch (Exception e) {
+                System.out.println("Banco de Dados Não Conectado");
+                e.printStackTrace();
+            }
+
+            return con;
+        }
 }
